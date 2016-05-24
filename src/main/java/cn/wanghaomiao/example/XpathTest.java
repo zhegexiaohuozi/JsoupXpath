6808ccd04571d69485b5fb5f4a7aac2434cb31f1
xiaohuo <et.tw@163.com>
2016-05-25 01:18:03 +0800

    测试案例统一移到测试用例中来

--- src/main/java/cn/wanghaomiao/example/XpathTest.java
+++ src/main/java/cn/wanghaomiao/example/XpathTest.java
@@ -7,5 +7,6 @@ package cn.wanghaomiao.example;
 public class XpathTest {
     public static void main(String[] args) {
         //测试案例统一挪到 test/java/cn.wanghaomiao.xpath.model.JXDocumentTest中，并根据测试页面改版情况更新了最新的XPath
+        //github地址：https://github.com/zhegexiaohuozi/JsoupXpath/blob/master/src/test/java/cn/wanghaomiao/xpath/model/JXDocumentTest.java
     }
 }
