9a827cbc994b425faef96a105cb523eb3f6646e0
xiaohuo <et.tw@163.com>
2021-11-30 19:32:57 +0800

    v2.5.1

--- src/main/java/org/seimicrawler/xpath/core/function/Sum.java
+++ src/main/java/org/seimicrawler/xpath/core/function/Sum.java
@@ -10,7 +10,6 @@ import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Function: number sum(node-set)
